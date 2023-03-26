378a7a9cfd96983c5f537aaaebd21e62e6305d14
Jefferson Dias <jeffersondcardoso@hotmail.com>
2023-03-26 16:33:03 -0300

    Implementação da Página do Usuário - 0.1

--- src/Alumni.php
+++ src/Alumni.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+$usuario = $_SESSION['usuario'];
 
 // Verifique se o usuário está logado
 if (!isset($_SESSION['usuario'])) {
@@ -9,18 +10,22 @@ if (!isset($_SESSION['usuario'])) {
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Actor">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>PORTAL | ALUMNI FACOM</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="assets/css/Footer-Basic.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/6.4.8/swiper-bundle.min.css">
-    <link rel="stylesheet" href="assets/css/Login-Form-Basic-icons.css">
 </head>
 
-<body style="color: #839eef;background: url(&quot;assets/img/background.png&quot;), #212b58;">
+<body style="color: #839eef; background: url(&quot;assets/img/background.png&quot;), #212b58;">
 
     <nav class="navbar navbar-light navbar-expand-md" style="background: #f2f2f2;font-family: Roboto;">
         <div class="container-fluid"><a class="navbar-brand" href="#" style="margin-left: 45px;">ALUMNI FACOM
@@ -28,22 +33,46 @@ if (!isset($_SESSION['usuario'])) {
             <div class="collapse navbar-collapse" id="navcol-1" style="background: #f2f2f2;">
                 <ul class="navbar-nav">
                     <li class="nav-item"><a class="nav-link" href="Alumni.php">Início</a></li>
-                </ul><a class="btn btn-danger btn-sm ms-auto" role="button" id='botao-logout' href="Login.html">Sair</a>
+                </ul>
+                <a class="btn btn-danger btn-sm ms-auto" role="button" id='botao-logout' href="Login.html">Sair</a>
+
                 </ul>
             </div>
         </div>
     </nav>
 
-    <div style="height: 600px; font-family: Roboto;">
-        <div class="card" style="width: 18rem; margin-top: 40px;margin-left:40px">
-            <div class="card-body">
-                <p class="card-text">Em Manutenção</p>
+    <!-- Start: Cards -->
+    <div class="row row-cols-1 row-cols-md-3 g-4" style="margin:40px;">
+        <div class="col">
+            <div class="card h-100">
+                <img src="Portal/img/Card-Busca_PNG.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
             </div>
         </div>
+        <div class="col">
+            <div class="card h-100">
+                <img src="Portal/img/Card-Forum_PNG.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card h-100">
+                <img src="Portal/img/Card-Oportunidades_PNG.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                </div>
+            </div>
+        </div>
+
     </div>
-
-
-
+    <!-- End: Cards -->
 
     <!-- Start: Footer Basic -->
     <footer class="footer-basic" style="padding-bottom: 15px;padding-top: 30px;">
